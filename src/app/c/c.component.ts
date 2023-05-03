6cc53ea97e9a8b0d1a7283c96135da26a3aa0b2f
oumaima saidi <73590711+sdoumaima@users.noreply.github.com>
2023-05-03 10:48:11 +0100

    Update c.component.ts

--- src/app/c/c.component.ts
+++ src/app/c/c.component.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
 
+
+
+
+export class AppModule { }
+
 @Component({
   selector: 'app-c',
   templateUrl: './c.component.html',
   styleUrls: ['./c.component.css']
 })
 export class CComponent {
+  exp: any = [];
+  formation: any = [];
+  langue: any = [];
+  university: any = [];
 
+
+  addlangue() {
+    this.langue.push(
+      {
+        langue: "",
+        niveau: ""
+      }
+    )
+  }
+
+  addexp(){
+    this.exp.push(
+      {
+        entreprise: "",
+        city: "",
+        fonction:"",
+        debutmois: "",
+        debutannee: "",
+        finmois: "",
+        finannee: "",
+        details: ""
+      }
+    )
+  }
+
+  adduniversity(){
+    this.university.push(
+      {
+        fac: "",
+        diploma: "",
+        debutmois: "",
+        debutannee: "",
+
+      }
+    )
+  }
+
+  addformation(){
+    this.formation.push(
+      {
+        centre: "",
+        diploma: "",
+        debutmois: "",
+        debutannee: "",
+
+      }
+    )
+  }
 
 }
 
